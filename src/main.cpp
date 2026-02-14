67318875a9e95830a919519989e0267218218bbf
brandonviaje <viajebrandon@gmail.com>
2026-02-14 13:42:27 -0500

    refactor: clean up main

--- src/main.cpp
+++ src/main.cpp
@@ -2,97 +2,24 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include "../include/token.hpp"
- 
-// convert characters into tokens
-std::vector<semp::Token> tokenize(const std::string& str) 
-{
-    std::vector<semp::Token> tokens;
-    std::string buffer;
+#include "../include/lexer.hpp"
 
-    std::size_t i {};
-    while (i < str.length()) {
-        char c = str[i];
-
-        // skip whitespace
-        if (std::isspace(c)) 
-        {
-            i++;
-            continue;
-        }
-
-        // semicolon
-        if (c == ';') 
-        {
-            tokens.push_back({semp::TokenType::_SEMICOLON, ";", 0});
-            i++;
-            continue;
-        }
-
-        // identifier / keyword
-        if (std::isalpha(c)) 
-        {
-            buffer.clear();
-            while (i < str.length() && std::isalpha(str[i])) {
-                buffer.push_back(str[i]);
-                i++;
-            }
-
-            // check for keywords
-            if (buffer == "ret") 
-            {
-                tokens.push_back({semp::TokenType::_RETURN, buffer, 0});
-            } 
-            else 
-            {
-                std::cerr << "Unknown identifier: " << buffer << std::endl;
-                exit(EXIT_FAILURE);
-            }
-            continue;
-        }
-
-        // integer literal
-        if (std::isdigit(c)) 
-        {
-            buffer.clear();
-
-            // keep scanning if char is still a digit
-            while (i < str.length() && std::isdigit(str[i])) 
-            {
-                buffer.push_back(str[i]);
-                i++;
-            }
-
-            tokens.push_back({semp::TokenType::_INT_LITERAL, buffer, 0});  // add to tokens
-            continue;
-        }
-
-        std::cerr << "Unexpected character: " << c << std::endl; // unknown character
-        exit(EXIT_FAILURE);
-    }
-
-    return tokens;
-}
-
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
     // catch args
-    if(argc != 2)
+    if (argc != 2)
     {
         std::cout << "Usage: semp <text.semp>" << '\n';
         return EXIT_FAILURE;
     }
 
-    std::fstream input(argv[1], std::ios::in);
-    std::stringstream buffer_stream;
-    
-    buffer_stream << input.rdbuf();         // load file into buffer
-    input.close();
+    // create lexer object that reads .semp file
+    semp::Lexer lexer(static_cast<std::string>(argv[1]));
 
-    std::string contents {buffer_stream.str()};
-    std::vector<semp::Token> tokens {tokenize(contents)};
+    // tokenize source file
+    std::vector<semp::Token> tokens{lexer.tokenize()};
 
-    for (const auto& element : tokens) 
+    for (const auto &element : tokens)
     {
         std::cout << element << '\n';
     }
