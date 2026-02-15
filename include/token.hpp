d35f1f0cc6119b696f7679eb507bea82f7488c2f
brandonviaje <viajebrandon@gmail.com>
2026-02-15 12:20:45 -0500

    perf: switch to string_view, add EOF and identifier tokens

--- include/token.hpp
+++ include/token.hpp
@@ -1,22 +1,25 @@
 #pragma once
 #include <ostream>
+#include <string_view>
 
 namespace semp
 {
     enum class TokenType
     {
         _RETURN,
+        _IDENTIFIER,
         _INT_LITERAL,
         _STR_LITERAL,
-        _LEFT_PARENS,
-        _RIGHT_PARENS,
-        _SEMICOLON
+        _LEFT_BRACE,
+        _RIGHT_BRACE,
+        _SEMICOLON,
+        _EOF
     };
 
     struct Token
     {
         TokenType type;
-        std::string lexeme;
+        std::string_view lexeme;
         int line;
     };
 
@@ -24,20 +27,16 @@ namespace semp
     {
         switch (type)
         {
-        case TokenType::_RETURN:
-            return "RETURN";
-        case TokenType::_INT_LITERAL:
-            return "INT_LITERAL";
-        case TokenType::_STR_LITERAL:
-            return "STR_LITERAL";
-        case TokenType::_LEFT_PARENS:
-            return "LEFT_PARENTHESES";
-        case TokenType::_RIGHT_PARENS:
-            return "RIGHT_PARENTHESES";
-        case TokenType::_SEMICOLON:
-            return "SEMICOLON";
+            case TokenType::_IDENTIFIER:    return "IDENTIFIER";
+            case TokenType::_RETURN:        return "RETURN";
+            case TokenType::_INT_LITERAL:   return "INT_LITERAL";
+            case TokenType::_STR_LITERAL:   return "STR_LITERAL";
+            case TokenType::_LEFT_BRACE:    return "LEFT_BRACE";
+            case TokenType::_RIGHT_BRACE:   return "RIGHT_BRACE";
+            case TokenType::_SEMICOLON:     return "SEMICOLON";
+            case TokenType::_EOF:           return "EOF";
+            default:                        return "UNKNOWN";
         }
-        return "UNKNOWN";
     }
 
     inline std::ostream &operator<<(std::ostream &os, const Token &token)
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -2,125 +2,86 @@
 #include <fstream>
 #include <sstream>
 
-// load source code into lexer
-std::string semp::Lexer::load_file(std::string file_path)
-{
-    std::fstream input(file_path, std::ios::in);
-    std::stringstream string_buffer;
-
-    string_buffer << input.rdbuf(); // load file into string buffer
-    input.close();
-
-    return string_buffer.str();
-}
-
 // convert characters into tokens
-std::vector<semp::Token> semp::Lexer::tokenize()
+std::vector<semp::Token> semp::Lexer::tokenize() 
 {
     std::vector<semp::Token> tokens;
-    std::string buffer;
     std::size_t i{};
+    int line{1}; 
 
-    // read through source file char by char
-    while (i < contents.length())
+    while (i < contents.length()) 
     {
-        char c{contents[i]};
+        char c {contents[i]};
 
-        // skip whitespace
-        if (std::isspace(c))
+        // skip white spaces
+        if (std::isspace(c)) 
         {
+            if (c == '\n') line++;
             i++;
             continue;
         }
 
-        // semicolon
-        if (c == ';')
+        std::size_t start {i}; // mark beginning of the token
+
+        // identifiers/keywords
+        if (std::isalpha(c)) 
         {
-            tokens.push_back({semp::TokenType::_SEMICOLON, ";", 0});
-            i++;
+            while (i < contents.length() && std::isalnum(contents[i])) i++;
+            std::string_view lexeme = std::string_view(contents).substr(start, i - start);
+            
+            // key words
+            TokenType type = (lexeme == "ret") ? TokenType::_RETURN : TokenType::_IDENTIFIER;
+            tokens.push_back({type, lexeme, line});
             continue;
         }
-
-        // parentheses
-        if (c == '{')
+        
+        // int literal
+        if (std::isdigit(c)) 
         {
-            tokens.push_back({semp::TokenType::_LEFT_PARENS, "{", 0});
-            i++;
-            continue;
-        }
-
-        if (c == '}')
-        {
-            tokens.push_back({semp::TokenType::_RIGHT_PARENS, "}", 0});
-            i++;
-            continue;
-        }
-
-        // identifier / keyword
-        if (std::isalpha(c))
-        {
-            buffer.clear();
-            while (i < contents.length() && std::isalpha(contents[i]))
-            {
-                buffer.push_back(contents[i]);
-                i++;
-            }
-
-            // check for keywords
-            if (buffer == "ret")
-            {
-                tokens.push_back({semp::TokenType::_RETURN, buffer, 0});
-            }
-            else
-            {
-                std::cerr << "Unknown Token: " << buffer << std::endl;
-                exit(EXIT_FAILURE);
-            }
+            while (i < contents.length() && std::isdigit(contents[i])) i++;
+            tokens.push_back({TokenType::_INT_LITERAL, std::string_view(contents).substr(start, i - start), line});
             continue;
         }
 
         // str literal
-        if (c == '"')
+        if (c == '"') 
         {
-            buffer.clear();
-            i++; // skip begining quote
+            i++; // skip opening quote
 
-            while (i < contents.length())
+            while (i < contents.length() && contents[i] != '"') 
             {
-                if (contents[i] == '"')
-                {
-                    i++; // skip closing quote
-                    break;
-                }
-                else
-                {
-                    buffer.push_back(contents[i]);
-                    i++;
-                }
-            }
-            tokens.push_back({semp::TokenType::_STR_LITERAL, buffer, 0}); // add to tokens
-            continue;
-        }
-
-        // integer literal
-        if (std::isdigit(c))
-        {
-            buffer.clear();
-
-            // keep scanning if char is still a digit
-            while (i < contents.length() && std::isdigit(contents[i]))
-            {
-                buffer.push_back(contents[i]);
+                if (contents[i] == '\n') line++;
                 i++;
             }
 
-            tokens.push_back({semp::TokenType::_INT_LITERAL, buffer, 0}); // add to tokens
+            // parse lexeme
+            std::string_view lexeme {std::string_view(contents).substr(start + 1, i - start - 1)}; 
+            
+            if (i < contents.length()) i++; // skip closing quote
+            else 
+            {
+                std::cerr << "Line " << line << ": Unterminated string literal\n";
+                exit(EXIT_FAILURE);
+            }
+            
+            tokens.push_back({TokenType::_STR_LITERAL, lexeme, line});
             continue;
         }
 
-        std::cerr << "Unexpected character: " << c << std::endl; // unknown character
+        // punctuation tokens
+        switch (c) 
+        {
+            case ';': tokens.push_back({TokenType::_SEMICOLON, ";", line}); i++; continue;
+            case '{': tokens.push_back({TokenType::_LEFT_BRACE, "{", line}); i++; continue;
+            case '}': tokens.push_back({TokenType::_RIGHT_BRACE, "}", line}); i++; continue;
+        }
+
+        std::cerr << "Line " << line << ": Unexpected character '" << c << "'\n";
         exit(EXIT_FAILURE);
     }
 
+    // EOF token
+    tokens.push_back({TokenType::_EOF, "", line});
+    
     return tokens;
 }
