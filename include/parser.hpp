ad4eb4c705a20c43e4d885bc4028e8244b3b3ef2
brandonviaje <viajebrandon@gmail.com>
2026-02-28 19:27:14 -0500

    style: apply clang format

--- include/parser.hpp
+++ include/parser.hpp
@@ -2,16 +2,18 @@
 #include <vector>
 #include "token.hpp"
 
-class Parser
-{
-    public:
-        Parser(std::vector<semp::Token> tokens);
-    private:
-        std::vector<semp::Token> m_tokens;
-        std::size_t m_current {};
-        
-        [[nodiscard]] bool isAtEnd() const;
-        [[nodiscard]]  semp::Token peek() const;
-        [[nodiscard]]  semp::Token previous() const;
-        semp::Token consume();
+class Parser {
+ public:
+  Parser(std::vector<semp::Token> tokens);
+
+ private:
+  std::vector<semp::Token> m_tokens;
+  std::size_t m_current{};
+
+  [[nodiscard]] bool isAtEnd() const;
+  [[nodiscard]] semp::Token peek() const;
+  [[nodiscard]] semp::Token previous() const;
+  semp::Token consume();
+  bool match(semp::TokenType type);
+  semp::Token expect(semp::TokenType type, const std::string& error_message);
 };
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,32 +1,48 @@
 #include "parser.hpp"
 
-Parser::Parser(std::vector<semp::Token> tokens) 
-    : m_tokens{std::move(tokens)}
-    {}
+Parser::Parser(std::vector<semp::Token> tokens) : m_tokens{std::move(tokens)} {}
 
-bool Parser::isAtEnd() const 
-{
-    // check if we hit EOF or end of vector
-    return m_current >= m_tokens.size() || m_tokens[m_current].type == semp::TokenType::_EOF; 
+bool Parser::isAtEnd() const {
+  // check if we hit EOF or end of vector
+  return m_current >= m_tokens.size() ||
+         m_tokens[m_current].type == semp::TokenType::_EOF;
 }
 
-semp::Token Parser::peek() const 
-{
-    return m_tokens[m_current];
+semp::Token Parser::peek() const {
+  return m_tokens[m_current];
 }
 
-semp::Token Parser::previous() const 
-{
-    return m_tokens[m_current - 1];
+semp::Token Parser::previous() const {
+  return m_tokens[m_current - 1];
 }
 
-semp::Token Parser::consume() 
-{
-    // check if token isnt the end
-    if (!isAtEnd()) 
-    {
-        m_current++;
-    }
+semp::Token Parser::consume() {
+  // check if token isnt the end
+  if (!isAtEnd()) {
+    m_current++;
+  }
 
-    return previous(); 
+  return previous();
+}
+
+bool Parser::match(semp::TokenType type) {
+  if (isAtEnd())
+    return false;  // if at end of vector, return false
+
+  // if token type matches desired type consume
+  if (peek().type == type) {
+    consume();
+    return true;
+  }
+
+  return false;
+}
+
+semp::Token Parser::expect(semp::TokenType type,
+                           const std::string& error_message) {
+  if (match(type)) {
+    return previous();
+  }
+
+  throw std::runtime_error(error_message);
 }
